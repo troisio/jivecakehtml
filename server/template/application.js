55cc5e91566e5410ab57bffa521289dfd397b8f5
Luis E. Banegas Saybe <luis@trois.io>
2017-12-12 18:45:28 -0500

    feat(event): Description persisted to header (#140)

--- server/template/application.js
+++ server/template/application.js
@@ -8,7 +8,7 @@ export default (page) => {
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-      <meta name="description" content="Event registration">
+      <meta name="description" content="${escapeHtml(page.description)}">
       <link rel="icon" href="/favicon.ico">
 
       <meta name="twitter:card" content="${escapeHtml(page.title)}">
